fix jerk computation corrections

examples/plot_kinematics.py
@@ -54,12 +54,13 @@ gridpoints = ta.interpolator.propose_gridpoints(
     max_iteration=500,
     min_nb_points=500,
 )
+gridpoints = np.linspace(0, 1, 10000)
 instance = algo.TOPPRA(
     [pc_vel, pc_acc],
     path,
     gridpoints=gridpoints,
-    parametrizer="ParametrizeConstAccel",
-    # parametrizer="ParametrizeSpline",
+    # parametrizer="ParametrizeConstAccel",
+    parametrizer="ParametrizeSpline",
 )
 jnt_traj = instance.compute_trajectory()
 assert jnt_traj is not None
@@ -69,7 +70,7 @@ jnt_traj = cast(ta.parametrizer.ParametrizeConstAccel, jnt_traj)
 # The output trajectory is an instance of
 # :class:`toppra.interpolator.AbstractGeometricPath`.
 
-jnt_traj.plot_parametrization(show=True)
+# jnt_traj.plot_parametrization(show=True)
 
 ts_sample = np.linspace(0, jnt_traj.duration, 100)
 qs_sample = jnt_traj(ts_sample)
@@ -87,19 +88,19 @@ for i in range(path.dof):
 from scipy.interpolate import CubicSpline
 
 # differentiate the spline
-cspline = CubicSpline(ts_sample, qs_sample, axis=0)
-spline = cspline(ts_sample, 0)
-d_spline = cspline(ts_sample, 1)
-dd_spline = cspline(ts_sample, 2)
-# ddd_spline = cspline(ts_sample, 3)
-sus_dd_spline = CubicSpline(ts_sample, qdds_sample, axis=0)
-ddd_spline = sus_dd_spline(ts_sample, 1)
+
+spline = qs_sample
+d_spline = np.diff(spline, axis=0) / np.diff(ts_sample)[:, None]
+dd_spline = np.diff(d_spline, axis=0) / np.diff(ts_sample[1:])[:, None]
+ddd_spline = np.diff(dd_spline, axis=0) / np.diff(ts_sample[2:])[:, None]
+# sus_dd_spline = CubicSpline(ts_sample, qdds_sample, axis=0)
+# ddd_spline = sus_dd_spline(ts_sample, 1)
 
 for i in range(path.dof):
     axs[0, 1].plot(ts_sample, spline[:, i], c="C{:d}".format(i))
-    axs[1, 1].plot(ts_sample, d_spline[:, i], c="C{:d}".format(i))
-    axs[2, 1].plot(ts_sample, dd_spline[:, i], c="C{:d}".format(i))
-    axs[3, 1].plot(ts_sample, ddd_spline[:, i], c="C{:d}".format(i))
+    axs[1, 1].plot(ts_sample[1:], d_spline[:, i], c="C{:d}".format(i))
+    axs[2, 1].plot(ts_sample[2:], dd_spline[:, i], c="C{:d}".format(i))
+    axs[3, 1].plot(ts_sample[3:], ddd_spline[:, i], c="C{:d}".format(i))
 
 axs[3, 0].set_xlabel("Time (s)")
 axs[3, 1].set_xlabel("Time (s)")

toppra/parametrizer.py
@@ -83,31 +83,41 @@ class ParametrizeConstAccel(AbstractGeometricPath):
         if isinstance(ts, (int, float)):
             ts = np.array([ts], dtype=float)
             scalar = True
-        ss, vs, us = self._eval_params(ts)
+        ss, vs, us, js = self._eval_params(ts)
+
         if order == 0:
+            # q(t) = q(s(t))
             out = self._path(ss)
         elif order == 1:
+            # q'(t) = q'(s(t)) * s'(t)
             out = np.multiply(self._path(ss, 1), vs[:, np.newaxis])
         elif order == 2:
+            # q''(t) = q''(s(t)) * s'(t)^2 + q'(s(t)) * s''(t)
             out = np.multiply(self._path(ss, 2), vs[:, np.newaxis] ** 2) + np.multiply(
                 self._path(ss, 1), us[:, np.newaxis]
             )
         elif order == 3:
+            # q'''(t) = q'(s(t)) * s'''(t) + 3 * q''(s(t)) * s'(t) * s''(t) + q'''(s(t)) * s'(t)^3
             # TODO: check if this is correct
-            out = np.multiply(
-                self._path(ss, 3), vs[:, np.newaxis] ** 3
-            ) + 3 * np.multiply(
-                self._path(ss, 2), vs[:, np.newaxis] ** 2 * us[:, np.newaxis]
+            out = (
+                np.multiply(self._path(ss, 1), js[:, np.newaxis])
+                + 3
+                * np.multiply(
+                    self._path(ss, 2), np.multiply(vs[:, np.newaxis], us[:, np.newaxis])
+                )
+                + np.multiply(self._path(ss, 3), vs[:, np.newaxis] ** 3)
             )
         else:
             raise ToppraError(f"Order {order} is not supported.")
         if scalar:
             return out[0]
+        logger.info(f"{ss.shape=}, {vs.shape=}, {us.shape=} {out.shape=}")
+
         return out
 
     def _eval_params(
         self, ts: np.ndarray
-    ) -> T.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    ) -> T.Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Return the array of path positions, velocities and accels.
 
         Parameters
@@ -118,7 +128,7 @@ class ParametrizeConstAccel(AbstractGeometricPath):
         Returns
         ---------
         :
-            Positions, velocity and accelerations.
+            Positions, velocity, accelerations and jerks
         """
         assert self._us is not None
         assert self._ts is not None
@@ -126,23 +136,31 @@ class ParametrizeConstAccel(AbstractGeometricPath):
         ss = []
         vs = []
         us = []
+        js = [0]
         for idx, t in zip(indices, ts):
             if idx == len(self._us):
                 idx -= 1
             dt = t - self._ts[idx]
+            if idx > 1:
+                js.append(
+                    (us[idx - 1] - us[idx - 2])
+                    * vs[idx - 1]
+                    / (self._ss[idx - 1] - self._ss[idx - 2])
+                )
             us.append(self._us[idx])
             vs.append(self._velocities[idx] + dt * self._us[idx])
             ss.append(
                 self._ss[idx] + dt * self._velocities[idx] + 0.5 * dt**2 * self._us[idx]
             )
-        return np.array(ss), np.array(vs), np.array(us)
+        js.append(0)
+        return np.array(ss), np.array(vs), np.array(us), np.array(js)
 
     def plot_parametrization(self, show: bool = False, n_sample: int = 500) -> None:
         """Plot the output parametrization and show it."""
 
         # small decrement to make sure all indices are valid
         ts = np.linspace(self.path_interval[0], self.path_interval[1], n_sample)
-        ss, vs, _ = self._eval_params(ts)
+        ss, vs, _, _ = self._eval_params(ts)
         qs = self(ts, 0)
 
         plt.subplot(2, 2, 1)