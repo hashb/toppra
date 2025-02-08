c87578c83f599bda8f78ba570835ad120ebc8fb0
Chenna Kautilya <chenna@outlook.com>
2025-02-08 12:24:19 -0800

    add topp3 files

---
+++ cpp/rossy_utils/thirdparty/clp/clpwrapper.hpp
@@ -0,0 +1,26 @@
+#include <Eigen/Dense>
+#include <coin/ClpSimplex.hpp>
+#include <coin/CoinPackedMatrix.hpp>
+
+class LPSolver {
+ public:
+  LPSolver();
+  ~LPSolver();
+  // min  f'x
+  // s.t. Ax < b
+  double solve(const Eigen::VectorXd& f, const Eigen::MatrixXd& A,
+               const Eigen::VectorXd& b, Eigen::VectorXd& x);
+
+  // min  f'x
+  // s.t. vlb < x  < vub
+  //       lb < Ax < ub
+  double solve(const Eigen::VectorXd& f, const Eigen::MatrixXd& A,
+               const Eigen::VectorXd& lb, const Eigen::VectorXd& ub,
+               const Eigen::VectorXd& vlb, const Eigen::VectorXd& vub,
+               Eigen::VectorXd& x);
+
+  CoinPackedMatrix from_EigenMatrix(const Eigen::MatrixXd& emat);
+
+ private:
+  ClpSimplex* model_;
+};
