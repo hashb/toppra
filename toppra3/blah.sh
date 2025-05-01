a821ec883f7fb032b4e664d5edf2a4413a37eee7
Chenna Kautilya <chenna@outlook.com>
2025-05-01 16:08:52 -0700

    wip

--- toppra3/blah.sh
+++ toppra3/blah.sh
@@ -1,14 +1,5 @@
-# this.BUILD_WITH_COLLISION_SUPPORT = "OFF";
-#   this.BUILD_WITH_CASADI_SUPPORT = "OFF";
-#   this.BUILD_WITH_AUTODIFF_SUPPORT = "OFF";
-#   this.BUILD_WITH_EXTRA_SUPPORT = "OFF";
-#   this.BUILD_WITH_CODEGEN_SUPPORT = "OFF";
-#   this.BUILD_WITH_OPENMP_SUPPORT = "OFF";
-#   this.BUILD_PYTHON_BINDINGS_WITH_BOOST_MPFR_SUPPORT = "OFF";
-#   this.INSTALL_DOCUMENTATION = "ON";
-#   this.GENERATE_PYTHON_STUBS = "ON";
-#   this.BUILD_WITH_ACCELERATE_SUPPORT = "OFF";
-#   this.BUILD_WITH_SDF_SUPPORT = "OFF";
+git clone --recursive https://github.com/stack-of-tasks/pinocchio.git -b v3.6.0
+cd pinocchio
 
 # install pixi
 curl -fsSL https://pixi.sh/install.sh | sh
