0fa1aeb98adfe5a63e009bfa046135e5593205a2
Chenna Kautilya <chenna@outlook.com>
2025-05-07 17:44:25 -0700

    wip

--- toppra3/blah.sh
+++ toppra3/blah.sh
@@ -13,6 +13,11 @@ cd /opt/build
 export CMAKE_PREFIX_PATH=/opt/install:$CMAKE_PREFIX_PATH
 export BOOST_ROOT=/opt/install
 
+# add to .bashrc
+echo "export CMAKE_PREFIX_PATH=/opt/install:$CMAKE_PREFIX_PATH" >> ~/.bashrc
+echo "export BOOST_ROOT=/opt/install" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/install/lib:/opt/install/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
+
 # Download Boost 1.68 source code
 wget https://archives.boost.io/release/1.68.0/source/boost_1_68_0.tar.gz
 
